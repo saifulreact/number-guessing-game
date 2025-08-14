daebe16488bf979b1cc3a552dfa8dd7cc558ba51
saifulreact <saifulreact@gmail.com>
2025-08-14 16:54:45 -0700

    update

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ button.addEventListener("click",()=>{
 
 
 })
+
+const man={
+    lastname:"islam",
+    firstnam:"siaful",
+    fullname:function(){
+        console.log(this.firstnam+ this.lastname)
+    }
+}
+man.fullname()
+
+console.log(man)
+
+const oli=["sai","islam","rakib","sakib","rani","bakir","rakib"]
+
+oli.push("hello")
+
+console.log(oli)
