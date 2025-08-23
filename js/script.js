a3e742c0e4eb6555ae5ab3f0e2df92f33254fb05
saifulreact <saifulreact@gmail.com>
2025-08-23 00:14:03 -0700

    doing half project

--- js/script.js
+++ js/script.js
@@ -1,25 +1,32 @@
-let heading=document.querySelector("h1")
-let input=document.querySelector("input")
-let button=document.querySelector("button")
+let headingone=document.querySelector(".heading")
+let inputone=document.querySelector(".input")
+let inputtwo=document.querySelector(".input_two")
+let buttonone=document.querySelector(".btn")
+let errorone=document.querySelector(".error")
 
-button.addEventListener("click",()=>{
+
+buttonone.addEventListener("click",()=>{
+
+    if(inputone.value==""  ){
+        errorone.innerHTML="Your number is not valide man"
+    }
+    if(!(inputone.value-0) ){
+          errorone.innerHTML="Enter a valid number man"
+    }
+    if(inputone.value>10||  inputone.value<1){
+        errorone.innerHTML="Your number is not valide man third "
+    }else{
+      headingone.innerHTML = "Player Two";
+inputone.style.display = "none";
+inputtwo.style.display = "block";
+
+    }
+    if(inputone.value=inputtwo.value){
+        headingone.innerHTML="Player one is winner"
+    }else{
+        headingone.innerHTML="Player two is winner "
+    }
+    
 
 
 })
-
-const man={
-    lastname:"islam",
-    firstnam:"siaful",
-    fullname:function(){
-        console.log(this.firstnam+ this.lastname)
-    }
-}
-man.fullname()
-
-console.log(man)
-
-const oli=["sai","islam","rakib","sakib","rani","bakir","rakib"]
-
-oli.push("hello")
-
-console.log(oli)
